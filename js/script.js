89e7924ff8bd17466630139a534917d0648c9d0b
Luca Isidoro <apprendista44@gmail.com>
2020-05-19 15:12:40 +0200

    Consegna esercizio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//NOME
+var nome = prompt('Inserisci il tuo nome:');
+console.log(nome);
+//COGNOME
+var cognome = prompt('Inserisci il tuo cognome');
+console.log(cognome);
+//COLORE PREFERITO
+var colore = prompt('Inserisci il tuo colore preferito');
+console.log(colore);
+//CREAZIONE PASSWORD
+document.writeln('La tua Password è: ' + nome + cognome + colore + '19');
